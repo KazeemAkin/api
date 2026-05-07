cff801e130e74265d601a871280c6d228b7160b2
AkinKazeem <w22075214@northumbria.ac.uk>
2026-05-07 11:10:07 +0100

    update

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { Callback } from "./api-liberaries/types/callback.data";
+// import { Callback } from "./api-liberaries/types/callback.data";
 
 import dotenv from "dotenv";
 import express from "express";
@@ -11,25 +11,28 @@ dotenv.config();
 // Config
 const app = express();
 
-const allowedOrigins = ["http://localhost:3001", "http://localhost:3000"];
+// const allowedOrigins = ["http://localhost:3001", "http://localhost:3000"];
 
 // Use CORS middleware with multiple origins
-app.use(
-  cors({
-    exposedHeaders: ["AccessToken"],
-    origin: function (origin: string | undefined, callback: Callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by the CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization", "AccessToken"],
-  }),
-);
+// app.use(
+//   cors({
+//     exposedHeaders: ["AccessToken"],
+//     origin: function (origin: string | undefined, callback: Callback) {
+//       // Allow requests with no origin (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by the CORS"));
+//       }
+//     },
+//     methods: ["GET", "POST", "PATCH", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization", "AccessToken"],
+//   }),
+// );
+app.use(cors({
+  origin: process.env.SITE_URL,
+}));
 
 // Body Parser middleware
 app.use(bodyParser.json({ limit: "100mb" }));
